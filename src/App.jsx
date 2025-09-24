6cbed88955c145f5053ef274c2fdb88408f2a66c
mukslima <marcos.nascimento.nl@gmail.com>
2025-09-24 14:59:16 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import dreamBeastLogo from './assets/Versão-rosacoral.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Olá, Dream Beast! <img src={dreamBeastLogo} alt="Logo Dream Beast" /></h1>
+      <p>sonhe melhor com formatura!</p>
+    </div>
+  )
+}
+
+export default App
