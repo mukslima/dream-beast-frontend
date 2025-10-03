57b33165adf4d5e4bfbb0cf918598b766a7777f7
mukslima <marcos.nascimento.nl@gmail.com>
2025-10-03 07:38:28 -0300

    Corrige HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import LoginCliente from './pages/LoginCliente'
 import LoginAdmin from './pages/LoginAdmin'
